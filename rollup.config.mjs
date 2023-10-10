4fcc36a14462037f64051fa02a1c924e52c575df
Jakub Hrubý <hrubyy.jakub@gmail.com>
2023-10-10 21:24:24 +0200

    config updates

---
+++ rollup.config.mjs
@@ -0,0 +1,21 @@
+import typescript from '@rollup/plugin-typescript';
+import packageJson from './package.json' assert { type: 'json' };
+import dts from "rollup-plugin-dts";
+
+const EXTERNAL = [...Object.keys(packageJson.peerDependencies)];
+
+export default [{
+    input: 'src/index.ts', // Your library's entry point
+    output: [
+        {
+            dir: 'dist', // Output file for ESM format
+            format: 'esm'
+        },
+    ],
+    plugins: [
+        typescript({
+            tsconfig: "tsconfig.json"
+        })
+    ],
+    external: [...EXTERNAL]
+}];
